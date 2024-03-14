25f8cfd1526fd969473d8dab6024474e518d9d10
Cl00n212 <150240213+Cl00n212@users.noreply.github.com>
2024-03-14 22:45:19 +0300

    lab2.py

--- lab2.py
+++ lab2.py
@@ -1,17 +1,24 @@
+#!/usr/bin/env python3
 import ev3dev2.motor as motor
 import time
 
-zadergka = 4
-zamer = 3
 motor_a = motor.LargeMotor(motor.OUTPUT_A)
-for volt in range(15, 51, 5):
-    time_start = time.time()
-    pos_start = motor_a.position
+for volt in range(-50, 51, 5):
+    if -50 <= volt <= -10 or 10 <= volt <= 50:
+        time_start = time.time()
+        pos_start = motor_a.position
+        name = "data" + str(volt) + ".txt"
 
-    while True:
-        timeReal = time.time() - time_start
-        motor_a.run_direct(duty_cycle_sp = volt)
-        if timeReal > zamer:
-            motor_a.run_direct(duty_cycle_sp = 0)
-            break
-    time.sleep(zadergka)
+        file = open(name, "w")
+        while True:
+            timeReal = time.time() - time_start
+            motor_a.run_direct(duty_cycle_sp = volt)
+            motor_pose = motor_a.position - pos_start
+            motor_vel = motor_a.speed
+            file.write(str(timeReal) + " " + str(motor_pose) + " " + str(motor_vel) + "\n")
+            if timeReal > 1:
+                motor_a.run_direct(duty_cycle_sp = 0)
+                break
+
+        file.close()
+        time.sleep(1)
