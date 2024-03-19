502faf2178f5d0d22a91187b9866579d4c40ed8d
Cl00n212 <150240213+Cl00n212@users.noreply.github.com>
2024-03-19 21:34:28 +0300

    Update lab4_vpd.py

--- lab4_vpd.py
+++ lab4_vpd.py
@@ -2,24 +2,51 @@ import math
 
 r_kol = 1 # радиус колёс
 B_rol = 5 # растояние между центрами двух колёс
-alph = 0.0 # изначальный угол относительно Ox
+theta = 0.0 # изначальный угол относительно Ox
 
 mass_proizv_cord = [0.0, 0.0, 0.0]
 mass_proizv_cord_prev = [0.0, 0.0, 0.0]
-mass_cord = [0.0, 0.0, alph]
+mass_cord = [0.0, 0.0, theta]
 
-def new_proiz(mass_wlr, mass_proizv_cord, mass_cord):
+def stat_motor(Ul,Ur):
+    
+
+def new_proiz(mass_wlr, mass_proizv_cord_prev, mass_proizv_cord, mass_cord):
     w = (mass_wlr[1] - mass_wlr[0]) * r_kol/ B_rol
     v = (mass_wlr[1] - mass_wlr[0]) * r_kol / 2
-    x_pi = v * math.cos(mass_cord[2])
-    y_pi = v * math.sin(mass_cord[2])
+    x_pi = v * math.cos(mass_cord[-1][2])
+    y_pi = v * math.sin(mass_cord[-1][2])
     th_pi = w
-    mass_proizv_cord.append([x_pi, y_pi, th_pi])
+    return (mass_proizv_cord, [x_pi, y_pi, th_pi])
+    
+def integrate(mass_cord,h, mass_proizv_cord_prev,mass_proizv_cord ):
+    mass_cord[0]=mass_cord[0]+(mass_proizv_cord_prev[0]+mass_proizv_cord[0])*h*0,5
+    mass_cord[1]=mass_cord[1]+(mass_proizv_cord_prev[1]+mass_proizv_cord[1])*h*0,5
+    mass_cord[2]=mass_cord[2]+(mass_proizv_cord_prev[2]+mass_proizv_cord[2])*h*0,5
+    return mass_cord
 
+def F_U(mass_gelaem, mass_cord): #3
+    motor_left = motor.LargeMotor(motor.OUTPUT_A)
+    motor_right = motor.LargeMotor(motor.OUTPUT_B)
 
+    time_last = time.time()
 
-
-
+    pos_start = [motor_left.position, motor_right.position]
+    
+    while True:
+        time_real = time.time()
+        h = time_real - time_last
+        
+        mass_wlr = [motor_left.speed, motor_right.speed]
+        
+    
+    mod_p = ((mass_gelaem[0] - mass_cord[0]) ** 2 + (mass_gelaem[1] - mass_cord[1]) ** 2) ** (1 / 2)
+    alfa = mass_gelaem[2] - mass_cord[2]
+    Us = Ks * mod_p
+    Ur = Kr * alfa
+    U_left = Us + Ur
+    U_right = Us - Ur
+    
 
 
 
