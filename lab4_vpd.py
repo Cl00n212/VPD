67c2c92cf0ec70baa8bb7f5962bd829f90940415
Cl00n212 <150240213+Cl00n212@users.noreply.github.com>
2024-04-01 22:34:24 +0300

    Update lab4_vpd.py

--- lab4_vpd.py
+++ lab4_vpd.py
@@ -1,97 +1,144 @@
+import ev3dev2.motor as motor
+import time
 import math
 
 r_kol = 1 # радиус колёс
 B_rol = 5 # растояние между центрами двух колёс
 theta = 0.0 # изначальный угол относительно Ox
+error = 0.05 # константы ...
+Ks = 0.2 # константы для прямолинейного движения 
+Kr = 0.2 # константы для криволинейного движения
+mass_start_cord = [0.0, 0.0, theta]
+mass_start_proizv = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
 
-mass_proizv_cord = [0.0, 0.0, 0.0]
-mass_proizv_cord_prev = [0.0, 0.0, 0.0]
-mass_cord = [0.0, 0.0, theta]
-
-def stat_motor(Ul,Ur):
-    
-
-def new_proiz(mass_wlr, mass_proizv_cord_prev, mass_proizv_cord, mass_cord):
-    w = (mass_wlr[1] - mass_wlr[0]) * r_kol/ B_rol
-    v = (mass_wlr[1] - mass_wlr[0]) * r_kol / 2
-    x_pi = v * math.cos(mass_cord[-1][2])
-    y_pi = v * math.sin(mass_cord[-1][2])
-    th_pi = w
-    return (mass_proizv_cord, [x_pi, y_pi, th_pi])
-    
-def integrate(mass_cord,h, mass_proizv_cord_prev,mass_proizv_cord ):
-    mass_cord[0]=mass_cord[0]+(mass_proizv_cord_prev[0]+mass_proizv_cord[0])*h*0,5
-    mass_cord[1]=mass_cord[1]+(mass_proizv_cord_prev[1]+mass_proizv_cord[1])*h*0,5
-    mass_cord[2]=mass_cord[2]+(mass_proizv_cord_prev[2]+mass_proizv_cord[2])*h*0,5
-    return mass_cord
-
-def F_U(mass_gelaem, mass_cord): #3
+def get_motors():
     motor_left = motor.LargeMotor(motor.OUTPUT_A)
     motor_right = motor.LargeMotor(motor.OUTPUT_B)
 
-    time_last = time.time()
+    return [motor_left, motor_right]
 
-    pos_start = [motor_left.position, motor_right.position]
-    
-    while True:
-        time_real = time.time()
-        h = time_real - time_last
-        
-        mass_wlr = [motor_left.speed, motor_right.speed]
-        
-    
+
+def stop_motors():
+    motors = get_motors()
+    motors[0].run_direct(duty_cycle_sp = 0)
+    motors[1].run_direct(duty_cycle_sp = 0)
+    return
+
+
+def stat_motors(Ul, Ur):
+    motors = get_motors()
+    motors[0].run_direct(duty_cycle_sp = Ul)
+    motors[1].run_direct(duty_cycle_sp = Ur)
+    return
+
+
+def get_U(mass_gelaem, mass_cord):
     mod_p = ((mass_gelaem[0] - mass_cord[0]) ** 2 + (mass_gelaem[1] - mass_cord[1]) ** 2) ** (1 / 2)
     alfa = mass_gelaem[2] - mass_cord[2]
     Us = Ks * mod_p
     Ur = Kr * alfa
     U_left = Us + Ur
     U_right = Us - Ur
+    return (U_left, U_right)
+
+
+
+
+
+def anpack_mass_proizv(mass_proizv):
+    return (mass_proizv[0], mass_proizv[1])
+
+
+def update_mass_proizv(mass_proizv, new_mass_proizv):
+    mass_proizv = new_mass_proizv
+    return
+
+
+def update_mass_cord(mass_cord, new_mass_cord):
+    mass_cord = new_mass_cord
+    return
+
+
+def new_proiz(mass_wlr, mass_proizv, mass_cord): #1
+
+    mass_proizv_cord_prev, mass_proizv_cord = anpack_mass_proizv(mass_proizv)
+
+    w = (mass_wlr[1] - mass_wlr[0]) * r_kol/ B_rol
+    v = (mass_wlr[1] - mass_wlr[0]) * r_kol / 2
+    x_pi = v * math.cos(mass_cord[-1][2])
+    y_pi = v * math.sin(mass_cord[-1][2])
+    th_pi = w
+
+    new_mass_proizv = [mass_proizv_cord, [x_pi, y_pi, th_pi]]
+
+    update_mass_proizv(mass_proizv, new_mass_proizv)
+
+    return 
+
+
+def integrate(mass_cord, mass_proizv, h): #2
+
+    mass_proizv_cord_prev, mass_proizv_cord = anpack_mass_proizv(mass_proizv)
+
+    new_mass_cord = []
+    new_mass_cord[0] = mass_cord[0] + (mass_proizv_cord_prev[0] + mass_proizv_cord[0]) * h * 0.5
+    new_mass_cord[1] = mass_cord[1] + (mass_proizv_cord_prev[1] + mass_proizv_cord[1]) * h * 0.5
+    new_mass_cord[2] = mass_cord[2] + (mass_proizv_cord_prev[2] + mass_proizv_cord[2]) * h * 0.5
+
+    update_mass_cord(mass_cord, new_mass_cord)
+
+    return 
+
+
+
+
+
+def check_error_cord(mass_cord_1,mass_cord_2):
+    if (abs(mass_cord_1[0] - mass_cord_2[1]) < error):
+        return True
+    else:
+        return False
+
+
+def F_U(mass_gelaem, mass_cord, mass_proizv): #3
+
+    motors = get_motors()
+
+    time_last = time.time()
+
+    pos_start = [motors[0].position, motors[1].position]
     
+    while True:
+        if (check_error_cord(mass_cord, mass_gelaem)):
+            break
+        mass_wlr = [motors[0].speed, motors[1].speed]
+
+        new_proiz(mass_wlr, mass_proizv, mass_cord)
+
+        time_real = time.time()
+        h = time_real - time_last
+        integrate(mass_cord, mass_proizv, h)
+
+        stat_motors(get_U(mass_gelaem, mass_cord))
+        
+        time_last = time_real
+    
+    stop_motors()
+
+    return
 
 
 
 
 
 
-'''
-def F_psi_start(x0,y0,xf,yf):
-    return atan2(yf - y0, xf - x0)
+n = 4
+for i in range(n):
+    x0, y0 = map(int, input().split())
 
-def input_psi():
-    #asdasasdasdasd
-    return [psi_l, psi _ r]
+    mass_cord = mass_start_cord
+    mass_proizv = mass_start_proizv
 
-def F_delta_L(delta_psi_l, delta_psi_r):
-    delta_L = (delta_psi_l * r + delta_psi_r * r) / 2
-    return delta_L
+    F_U([x0, y0], mass_cord, mass_proizv)
 
-def F_delta_omega(delta_psi_l, delta_psi_r):
-    delta_omega = (delta_psi_r + delta_psi_l) * r / B
-    return delta_omega
-
-def Delta_cordinat(delta_psi_l, delta_psi_r, last_omega):
-    delta_omega = F_delta_omega(delta_psi_l, delta_psi_r)
-    real_omega = last_omega + delta_omega
-    delna_x = math.cos(real_omega) * F_delta_L(delta_psi_l, delta_psi_r)
-    delta_y = math.sin(real_omega) * F_delta_L(delta_psi_l, delta_psi_r)
-    return [delna_x, delta_y, delta_omega]
-
-def Get_real_cordinat(delta_psi_l, delta_psi_r, last_x, last_y, last_omega):
-    mass_delta = Delta_cordinat(delta_psi_l, delta_psi_r, last_omega)
-    real_x = last_x + mass_delta[0]
-    real_y = last_u + mass_delta[0]
-    real_omega = last_omega + mass_delta[0]
-    return [real_x, real_y, real_omega]
-
-def F_U(mass_gelaem, mass_real_cordinat): #3
-    mod_p = ((mass_gelaem[0] - mass_real_cordinat[0]) ** 2 + (mass_gelaem[1] - mass_real_cordinat[1]) ** 2) ** (1 / 2)
-    alfa = mass_gelaem[2] - mass_real_cordinat[2]
-    Us = Ks * mod_p
-    Ur = Kr * alfa
-    return [Us + Ur, Us - Ur]
-
-def fun():
-
-
-fun()
-'''
+print("Программа закончила работу")
