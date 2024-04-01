7efe4693643420ef046606ece12495a87f933eea
Cl00n212 <150240213+Cl00n212@users.noreply.github.com>
2024-04-01 23:22:54 +0300

    Update lab4_vpd.py

--- lab4_vpd.py
+++ lab4_vpd.py
@@ -11,50 +11,61 @@ Kr = 0.2 # константы для криволинейного движени
 mass_start_cord = [0.0, 0.0, theta]
 mass_start_proizv = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
 
-def get_motors():
+def get_motors(): # возвращяет массив [левый_двигатель, правый_двигатель]
     motor_left = motor.LargeMotor(motor.OUTPUT_A)
     motor_right = motor.LargeMotor(motor.OUTPUT_B)
 
     return [motor_left, motor_right]
 
 
-def stop_motors():
+def stop_motors(): # останавливает моторы
     motors = get_motors()
     motors[0].run_direct(duty_cycle_sp = 0)
     motors[1].run_direct(duty_cycle_sp = 0)
     return
 
 
-def stat_motors(Ul, Ur):
+def start_motors(Ul, Ur): # запускает моторы с наряжением Ul и Ur
     motors = get_motors()
     motors[0].run_direct(duty_cycle_sp = Ul)
     motors[1].run_direct(duty_cycle_sp = Ur)
     return
 
 
-def get_U(mass_gelaem, mass_cord):
+def get_U(mass_gelaem, mass_cord): # возвращает напряжение картежем (U_left, U_right)
     mod_p = ((mass_gelaem[0] - mass_cord[0]) ** 2 + (mass_gelaem[1] - mass_cord[1]) ** 2) ** (1 / 2)
     alfa = mass_gelaem[2] - mass_cord[2]
     Us = Ks * mod_p
     Ur = Kr * alfa
     U_left = Us + Ur
     U_right = Us - Ur
+    
+    if (U_left > 100):
+        U_left = 100
+    elif (U_left < -100):
+        U_left = -100
+
+    if (U_right > 100):
+        U_right = 100
+    elif (U_right < -100):
+        U_right = -100
+
     return (U_left, U_right)
 
 
 
 
 
-def anpack_mass_proizv(mass_proizv):
+def anpack_mass_proizv(mass_proizv): # возвращает mass производных картежем (mass_proizv_last, mass_proizv_real)
     return (mass_proizv[0], mass_proizv[1])
 
 
-def update_mass_proizv(mass_proizv, new_mass_proizv):
+def update_mass_proizv(mass_proizv, new_mass_proizv): # обновляет значение первой переменной(mass_proizv) на занченние второй
     mass_proizv = new_mass_proizv
     return
 
 
-def update_mass_cord(mass_cord, new_mass_cord):
+def update_mass_cord(mass_cord, new_mass_cord): # обновляет значение первой переменной(mass_cord) на занченние второй
     mass_cord = new_mass_cord
     return
 
@@ -93,7 +104,7 @@ def integrate(mass_cord, mass_proizv, h): #2
 
 
 
-def check_error_cord(mass_cord_1,mass_cord_2):
+def check_error_cord(mass_cord_1, mass_cord_2): # проверяет отличается ли значение кодинат на error
     if (abs(mass_cord_1[0] - mass_cord_2[1]) < error):
         return True
     else:
@@ -119,7 +130,7 @@ def F_U(mass_gelaem, mass_cord, mass_proizv): #3
         h = time_real - time_last
         integrate(mass_cord, mass_proizv, h)
 
-        stat_motors(get_U(mass_gelaem, mass_cord))
+        start_motors(get_U(mass_gelaem, mass_cord))
         
         time_last = time_real
     
@@ -138,7 +149,7 @@ for i in range(n):
 
     mass_cord = mass_start_cord
     mass_proizv = mass_start_proizv
-
+    
     F_U([x0, y0], mass_cord, mass_proizv)
 
 print("Программа закончила работу")
